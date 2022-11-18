4657430c8f3e570b72c53bc67a364baf32af0dda
Backwards <deema.light@gmail.com>
2022-11-19 01:13:06 +0300

    render board->squares + css

---
+++ src/index.js
@@ -0,0 +1,76 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+
+class Board extends React.Component {
+    renderSquare(i) {
+        return <Square value={i} />
+    }
+
+    render() {
+        const status = 'Next player: X'
+
+        return (
+            <div>
+                <div className='status'>{status}</div>
+
+                <div className='board-row'>
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                    {this.renderSquare(3)}
+                </div>
+                <div className='board-row'>
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                    {this.renderSquare(6)}
+                </div>
+                <div className='board-row'>
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                    {this.renderSquare(9)}
+                </div>
+            </div>
+        )
+    }
+}
+
+class Square extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            value: null
+        }
+    }
+
+    render () {
+        return (
+            <button 
+            className='square' 
+            onClick={() => this.setState({value: 'X'})}
+            >
+                {this.state.value}
+            </button>
+        )
+    }
+}
+
+class Game extends React.Component {
+    render () {
+        return (
+            <div className='game'>
+                <div className='game-board'>
+                    <Board />
+                </div>
+                <div className='game-info'>
+                    <div>status</div>
+                    <ol>todo...</ol>
+                </div>
+            </div>
+        )
+    }
+}
+
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(<Game />)
