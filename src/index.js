01cf91d0df721fcc075c715455e9c86693cc659a
Backwards <deema.light@gmail.com>
2022-11-19 01:34:32 +0300

    square handleClick

--- src/index.js
+++ src/index.js
@@ -3,8 +3,24 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 
 class Board extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            squares: Array(9).fill(null)
+        }
+    }
+
+    handleClick(i) {
+        const squares = this.state.squares.slice()
+        squares[i] = 'X'
+        this.setState({squares: squares})
+    }
+
     renderSquare(i) {
-        return <Square value={i} />
+        return <Square 
+        value={this.state.squares[i]} 
+        onClick={() => this.handleClick(i)}
+        />
     }
 
     render() {
@@ -35,20 +51,13 @@ class Board extends React.Component {
 }
 
 class Square extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            value: null
-        }
-    }
-
     render () {
         return (
             <button 
             className='square' 
-            onClick={() => this.setState({value: 'X'})}
+            onClick={() => this.props.onClick()}
             >
-                {this.state.value}
+                {this.props.value}
             </button>
         )
     }
