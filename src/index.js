803e21c6fcdd20598534e78726699e371d0abf74
Backwards <deema.light@gmail.com>
2022-11-19 13:11:46 +0300

    square class -> function

--- src/index.js
+++ src/index.js
@@ -50,17 +50,15 @@ class Board extends React.Component {
     }
 }
 
-class Square extends React.Component {
-    render () {
-        return (
-            <button 
-            className='square' 
-            onClick={() => this.props.onClick()}
-            >
-                {this.props.value}
-            </button>
-        )
-    }
+function Square (props) {
+    return (
+        <button 
+        className='square' 
+        onClick={props.onClick}
+        >
+            {props.value}
+        </button>
+    )
 }
 
 class Game extends React.Component {
